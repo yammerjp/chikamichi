c2576b22b425e535df691f6a6d606e21aa69474d
Keisuke Nakayama <me@yammer.jp>
2023-06-17 17:04:38 +0900

    use cobra

---
+++ main_org.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"os"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+const proxyHost = "api.openai.com"
+
+func getModifyRequest() func(req *http.Request) {
+	return func(req *http.Request) {
+		req.Header = http.Header{}
+		req.URL.Scheme = "https"
+		req.URL.Host = proxyHost
+		req.Host = proxyHost
+		req.Header.Set("Authorization", "Bearer "+os.Getenv("OPENAI_API_KEY"))
+		req.Header.Set("Content-Type", "application/json")
+	}
+}
+
+func getModifyResponse() func(*http.Response) error {
+	return func(res *http.Response) error {
+		res.Header = http.Header{}
+		appendAccessControllAllowHeaders(&res.Header)
+		return nil
+	}
+}
+
+func main() {
+	if len(os.Args) >= 2 && os.Args[1] == "--jwt-gen" {
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"iss": "my-auth-server",
+			"sub": "john",
+			"aud": "john",
+		})
+		s, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(s)
+
+		return
+	}
+
+	rp := &httputil.ReverseProxy{Director: getModifyRequest()}
+	rp.ModifyResponse = getModifyResponse()
+	server := http.Server{
+		Addr:    ":8081",
+		Handler: WithLogging(WithPreflight(WithAuthorization(rp))),
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
